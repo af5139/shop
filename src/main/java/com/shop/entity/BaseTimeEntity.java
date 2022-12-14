ce9fe6cc369605ccdcee2e4c6da2840faea3f7e7
이영웅 <gi5060@naver.com>
2022-12-15 00:17:57 +0900

    auditing test code add

--- src/main/java/com/shop/entity/BaseTimeEntity.java
+++ src/main/java/com/shop/entity/BaseTimeEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @EntityListeners(value = {AuditingEntityListener.class})//auditing을 적용하기 위한 어노테이션
 @MappedSuperclass//공통 매핑 정보가 필요할 때 사용하는 어노테이션으로 부모 클래스를 상속 받는 자식 클래스에 매핑 정보만 제공
 @Getter@Setter
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate//엔티티가 생성되어 저장될 때 시간을 자동으로 저장
     @Column(updatable = false)
--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Table(name = "orders")
 @Getter@Setter
 @ToString
-public class Order {
+public class Order extends BaseEntity{
 
     @Id @GeneratedValue
     @Column(name = "order_id")
@@ -34,9 +34,9 @@ public class Order {
             fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    private LocalDateTime regTime;
-
-    private LocalDateTime updateTime;
+//    private LocalDateTime regTime;
+//
+//    private LocalDateTime updateTime;
 
 
 
