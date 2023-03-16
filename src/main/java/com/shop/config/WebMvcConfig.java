8338531cc1186b6738b6dab1622ec20959076036
이영웅 <gi5060@naver.com>
2023-03-16 22:06:23 +0900

    메인화면추가

--- src/main/java/com/shop/config/WebMvcConfig.java
+++ src/main/java/com/shop/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**")
+        registry.addResourceHandler("/images/**")//url 에 이미지로 시작하는 경우 uploadpath 값 기준으로 파일을 읽어옴
                 .addResourceLocations(uploadPath);
     }
 }
